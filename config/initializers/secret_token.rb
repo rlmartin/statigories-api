5ce7957534c917a581fd8133f56df532cfe139c1
Ryan Martin <rlmartin@gmail.com>
2011-10-10 13:05:01 -0400

    Initial commit with base user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StatigoriesCom::Application.config.secret_token = '153ec5cd6957914fdd2cbcaca10aa2d84034e5f79a84db9d424899e3e6ae006f585930f737535ebabec772fab82d1b0ffd2f60ce40ee04004d80eb051f53baa6'
